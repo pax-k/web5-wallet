0d86f312847d0bb5e7098d38c5ad0a7f0fdb405d
Adam Mika <amika@squareup.com>
2023-05-15 14:26:27 -0600

    Add WebExtension target

---
+++ ios/WebExtension/SafariWebExtensionHandler.swift
@@ -0,0 +1,21 @@
+//
+// Copyright © Square, Inc. All rights reserved.
+//
+
+import SafariServices
+import os.log
+
+class SafariWebExtensionHandler: NSObject, NSExtensionRequestHandling {
+
+    func beginRequest(with context: NSExtensionContext) {
+        let item = context.inputItems[0] as! NSExtensionItem
+        let message = item.userInfo?[SFExtensionMessageKey]
+        os_log(.default, "Received message from browser.runtime.sendNativeMessage: %@", message as! CVarArg)
+
+        let response = NSExtensionItem()
+        response.userInfo = [ SFExtensionMessageKey: [ "Response to": message ] ]
+
+        context.completeRequest(returningItems: [response], completionHandler: nil)
+    }
+
+}
